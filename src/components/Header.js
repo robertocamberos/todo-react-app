30e02e1171bfd8c20319e36c066def0b0470e364
Roberto Camberos <42880184+rcamberos23@users.noreply.github.com>
2022-10-18 12:23:41 -0700

    Task Tracker with add button appears in browser

---
+++ src/components/Header.js
@@ -0,0 +1,72 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({title}) => {
+  const onClick = () => {
+    console.log('Click')
+}
+
+  return (
+    <header className='header'>
+      <h1>{title}</h1>
+      <Button color='green' text='Add' 
+      onClick = {onClick} />
+    </header>
+  )
+}
+
+Header.defaultProps = {
+  title: 'Task Tracker'
+}
+
+Header.propTypes = {
+  title: PropTypes.string,
+}
+
+export default Header
+
+// You can style stuff inline with variabels if you want to
+
+
+
+
+
+
+
+
+
+// import PropTypes from 'prop-types'
+// import { useLocation } from 'react-router-dom'
+// import Button from './Button'
+
+// const Header = ({title, onAdd, showAdd}) => {
+//   const location = useLocation()
+
+//   return (
+//     <header className='header'>
+//         <h1 >{title}</h1>
+//         {location.pathname === '/' && (
+//         <Button
+//           color={showAdd ? 'red' : 'green'}
+//           text={showAdd ? 'Close' : 'Add'}
+//           onClick={onAdd}
+//         />
+//       )}
+//     </header>
+//   )
+// }
+
+// Header.defaultProps = {
+//     title: 'Task Tracker'
+// }
+
+// Header.propTypes = {
+//     title: PropTypes.string.isRequired
+// }
+
+// // const headingStyle = {
+// //     color: 'red',
+// //     backgroundColor: 'black'
+// // }
+
+// export default Header
