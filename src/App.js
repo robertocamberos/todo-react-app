6e38e7eb8298a5835dde62a8735a182aa544ea75
Roberto Camberos <42880184+rcamberos23@users.noreply.github.com>
2022-10-18 15:57:51 -0700

    displays a message when all tasks removed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
+import {useState} from 'react';
 import Header from './components/Header';
+import Tasks from './components/Tasks';
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      "id": 1,
+      "text": "test",
+      "other": "no-show",
+      "day": "monday",
+      "reminder": true
+    },
+    {
+      "text": "Test4",
+      "day": "Wednesday",
+      "reminder": false,
+      "id": 3
+    },
+    {
+      "text": "Task6",
+      "day": "Thursday",
+      "reminder": true,
+      "id": 4
+    } ]
+  )
+    // Delete Task
+    const deleteTask = (id) => {
+      setTasks(tasks.filter((task) => task.id !== id))
+    }
+
+    // Toggle Reminder
+    const toggleReminder = (id) => {
+      console.log(id)
+    }
+
   return (
     <div className="container">
       <Header />
+      {tasks.length > 0 ?
+      <Tasks tasks={tasks} onDelete=
+      {deleteTask} onToggle={toggleReminder}/> : "No tasks to display"}
     </div>
-  )
-
+  );
 }
 
 export default App;
