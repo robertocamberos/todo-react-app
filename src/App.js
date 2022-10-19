eec3dc3888a5031b00854a11827ee9a17731cb8c
Roberto Camberos <42880184+rcamberos23@users.noreply.github.com>
2022-10-19 14:38:39 -0700

    working app

--- src/App.js
+++ src/App.js
@@ -1,45 +1,95 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 
 const App = () => {
-  const [tasks, setTasks] = useState([
-    {
-      "id": 1,
-      "text": "test",
-      "other": "no-show",
-      "day": "monday",
-      "reminder": true
-    },
-    {
-      "text": "Test4",
-      "day": "Wednesday",
-      "reminder": false,
-      "id": 3
-    },
-    {
-      "text": "Task6",
-      "day": "Thursday",
-      "reminder": true,
-      "id": 4
-    } ]
-  )
+  const [showAddTask, setShowAddTask] = 
+  useState(true)
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+    getTasks()
+
+  }, [])
+
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    return data
+  }
+
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+
+    return data
+  }
+  
+
+    // Add Task
+    const addTask = async (task) => {
+      const res = await fetch(`http://localhost:5000/tasks`, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(task)
+      })  
+
+      const data = await res.json()
+
+      setTasks([...tasks, data])
+
+
+      // const id = Math.floor(Math.random() * 10000) + 1
+      // const newTask = {id, ...task}
+      // setTasks([...tasks, newTask])
+    }
+    
     // Delete Task
-    const deleteTask = (id) => {
+    const deleteTask = async (id) => {
+      await fetch(`http://localhost:5000/tasks/${id}`, {
+        method: 'DELETE',
+      })
+
       setTasks(tasks.filter((task) => task.id !== id))
     }
 
     // Toggle Reminder
-    const toggleReminder = (id) => {
-      console.log(id)
+    const toggleReminder = async (id) => {
+      const taskToToggle = await fetchTask(id)
+
+      const updTask = {...taskToToggle,
+      reminder: !taskToToggle.reminder}
+
+      const res = await fetch(`http://localhost:5000/tasks/${id}` , {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(updTask)
+      })
+
+      const data = await res.json()
+
+      setTasks(tasks.map((task) => task.id === id? {...task, reminder: data.reminder} : task))
     }
 
   return (
     <div className="container">
-      <Header />
-      {tasks.length > 0 ?
-      <Tasks tasks={tasks} onDelete=
-      {deleteTask} onToggle={toggleReminder}/> : "No tasks to display"}
+      <Header onAdd={() => setShowAddTask 
+        (!showAddTask)} showAddTask=
+        {showAddTask} />
+      {showAddTask && <AddTask onAdd=
+      {addTask}/>}
+      {tasks.length > 0 ? <Tasks tasks=
+      {tasks} onDelete= {deleteTask} onToggle={toggleReminder}/> : "No tasks to display"}
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -2,7 +2,7 @@ import {FaTimes} from 'react-icons/fa'
 
 const Task = ({task, onDelete, onToggle}) => {
   return (
-    <div className="task" onDoubleClick={onToggle}>
+    <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() => onToggle(task.id)}>
       <h3>{task.text}  <FaTimes 
       style={{color:'red',
     cursor:'pointer'}}
@@ -13,6 +13,24 @@ const Task = ({task, onDelete, onToggle}) => {
 }
 
 export default Task
+
+// import {FaTimes} from 'react-icons/fa'
+// import {Link} from 'react-router-dom'
+
+// const Task = ({task, onDelete, onToggle}) => {
+//   return (
+//     <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() => onToggle(task.id)}>
+//         <h3>{task.text} <FaTimes style={{ color: 'red', cursor: 'pointer'}} onClick={() => onDelete(task.id)}/></h3>
+//         <p>{task.day}</p>
+//         <p><Link to={`/task/${task.id}`}>View Details</Link></p>
+//     </div>
+//   )
+// }
+
+// export default Task
+
+
+
 
 // import {FaTimes} from 'react-icons/fa'
 // import {Link} from 'react-router-dom'
